f6bbd0f5459da54502d2ae21176e921fc534b9a3
Jahanzeb <jahansimran244@gmail.com>
2022-10-11 17:48:38 +0000

    chrono-slider-react

---
+++ src/components/PlanetItem/index.js
@@ -0,0 +1,16 @@
+import './index.css'
+
+const PlanetItem = props => {
+  const {planetDetails} = props
+  const {name, imageUrl, description} = planetDetails
+
+  return (
+    <div className="planet-item-container">
+      <img src={imageUrl} alt={`planet ${name}`} className="planet-image" />
+      <h1 className="name">{name}</h1>
+      <p className="description">{description}</p>
+    </div>
+  )
+}
+
+export default PlanetItem
---
+++ src/components/PlanetsSlider/index.js
@@ -0,0 +1,22 @@
+import Slider from 'react-slick'
+
+import PlanetItem from '../PlanetItem'
+
+import './index.css'
+
+const PlanetsSlider = props => {
+  const {planetsList} = props
+
+  return (
+    <div className="planet-slider-container" testid="planets">
+      <h1 className="planet-heading ">PLANETS</h1>
+      <Slider>
+        {planetsList.map(each => (
+          <PlanetItem key={each.id} planetDetails={each} />
+        ))}
+      </Slider>
+    </div>
+  )
+}
+
+export default PlanetsSlider
